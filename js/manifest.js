4f85c9998e86a549fb7cf1a8cf2c4ee68bab500c
seemitx <oatphuchit04@gmail.com>
2026-06-22 10:22:49 +0700

    Create manifest.js

---
+++ js/manifest.js
@@ -0,0 +1,12 @@
+{
+  "background_color": "#000000",
+  "dir": "ltr",
+  "display": "standalone",
+  "name": "IMS System",
+  "orientation": "any",
+  "scope": "/",
+  "short_name": "IMS System",
+  "start_url": "https://seemitx.github.io/IMS-Stocker/",
+  "theme_color": "#ff0000",
+  "lang": "th"
+}
